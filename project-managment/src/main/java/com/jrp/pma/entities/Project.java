fcc43d131ad780325e1eda23c0200d23786df4bb
Praveen G <25690316+cod3cat@users.noreply.github.com>
2021-02-25 09:24:07 +0530

    Initial Commit

---
+++ project-managment/src/main/java/com/jrp/pma/entities/Project.java
@@ -0,0 +1,52 @@
+package com.jrp.pma.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Project {
+	private long projectId;
+	private String name;
+	private String stage; // NOT STARTED, COMPLETED, IN PROGRESS	
+	private String description;
+	
+	public Project() {
+		
+	}
+	
+	public Project(String name, String stage, String description) {
+		super();
+		this.name = name;
+		this.stage = stage;
+		this.description = description;
+	}
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	public long getProjectId() {
+		return projectId;
+	}
+	public void setProjectId(long projectId) {
+		this.projectId = projectId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getStage() {
+		return stage;
+	}
+	public void setStage(String stage) {
+		this.stage = stage;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+}
